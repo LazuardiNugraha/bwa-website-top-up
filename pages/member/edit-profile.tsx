6d6f13608766414a25ba6b37a1f495a1ff9f0dad
LazuardiNugraha <nugrahalazuardim@gmail.com>
2022-06-28 15:30:57 +0700

    Refactor navbar component

--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -8,7 +8,7 @@ export default function EditProfile() {
             <form action="">
               <div className="photo d-flex">
                 <div className="position-relative me-20">
-                  <img src="../assets/img/avatar-1.png" width="90" height="90" className="avatar img-fluid" />
+                  <img src="/img/avatar-1.png" width="90" height="90" className="avatar img-fluid" />
                   <div className="avatar-overlay position-absolute top-0 d-flex justify-content-center align-items-center">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M3 6H5H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
